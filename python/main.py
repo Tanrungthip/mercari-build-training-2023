1a534a1f40e63de3af203692a03d56425f5c3b2c
Rungthip Chaothayee <ttann.trc@gmail.com>
2023-05-11 18:17:59 +0900

    completed step 3

--- python/main.py
+++ python/main.py
@@ -2,7 +2,8 @@ import os
 import logging
 import pathlib
 import json
-from fastapi import FastAPI, Form, HTTPException
+import hashlib
+from fastapi import FastAPI, Form, HTTPException, File, UploadFile
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -24,17 +25,25 @@ def root():
     return {"message": "Hello, world!"}
 
 @app.post("/items")
-def add_item(name: str = Form(...), category: str = Form(...)):
+def add_item(name: str = Form(...), category: str = Form(...), image: UploadFile = File(...)):
     logger.info(f"Receive item: {name}, category: {category}")
+    # Hash image file, a string representing the SHA-256 image data
+    image_hash = hashlib.sha256(image.file.read()).hexdigest()
+    image_filename = f"{image_hash}.jpg"
+    # Save image file
+    image_path = images / image_filename
+    with open(image_path, "wb") as file:
+        file.write(image.file.read())
     # Open items.json file
     with open('items.json', 'r') as file:
         items = json.load(file)
     # Add new item to the items list
-    items['items'].append({"name": name, "category": category})
-    # Write updated items back to  file
+    items['items'].append({"name": name, "category": category, "image_filename" : image_filename})
+    # Updated items back to file
     with open('items.json', 'w') as file:
         json.dump(items, file)
-    return {"message": f"item received: {name}, category: {category} "}
+    # return {"items": items['items']}
+    return {"message": f"item received: {name}, category: {category}, image: {image_filename}"}
 
 @app.get("/items")
 def get_items():
@@ -42,6 +51,13 @@ def get_items():
     with open('items.json', 'r') as f:
         items = json.load(f)
     return items
+
+
+@app.get("/items/{item_id}")
+def get_item_id(item_id: int):
+    with open('items.json','r') as file:
+        items = json.load(file)
+    return items['items'][item_id]
 
 
 @app.get("/image/{image_filename}")
