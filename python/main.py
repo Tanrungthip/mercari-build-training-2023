30481e3ea1cf445f857d5f9c0133126727229978
Rungthip Chaothayee <ttann.trc@gmail.com>
2023-05-19 03:21:50 +0900

    add search function, create table method

--- python/main.py
+++ python/main.py
@@ -21,12 +21,40 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+db_path = "./db/mercari.sqlite3"
+
+def create_table(conn: sqlite3.Connection):
+    table = [
+        """
+        CREATE TABLE IF NOT EXISTS items (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            category_id INTEGER,
+            image_name TEXT NOT NULL,
+            FOREIGN KEY(category_id) REFERENCES category(id)
+        )
+        """,
+        """
+        CREATE TABLE IF NOT EXISTS category (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL
+        )
+        """
+    ]
+    try:
+        c = conn.cursor()
+        for statement in table:
+            c.execute(statement)
+        conn.commit()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+conn = sqlite3.connect(db_path)
+create_table(conn)
+
 @app.get("/")
 def root():
     return {"message": "Hello, world!"}
-
-data_base = "mercari.sqlite3"
-
 
 @app.post("/items")
 def add_item(name: str = Form(...), category: str = Form(...), image: UploadFile = File(...)):
@@ -34,83 +62,69 @@ def add_item(name: str = Form(...), category: str = Form(...), image: UploadFile
     # Hash image file, a string representing the SHA-256 image data
     image_hash = hashlib.sha256(image.file.read()).hexdigest()
     image_filename = f"{image_hash}.jpg"
-    # Save image file
-    image_path = images / image_filename
-    with open(image_path, "wb") as file:
-        file.write(image.file.read())
 
-    # Add item into items db
-    conn = sqlite3.connect('data_base')
+    conn = sqlite3.connect(db_path)
     c = conn.cursor()
-    c.execute("INSERT INTO items (name, category, image_name) VALUES (?, ?, ?)", (name, category, image_filename))
-    # Commit / Close command
+    c.execute("SELECT id FROM category WHERE name=?", (category,))
+    category_id = c.fetchone()
+
+    if category_id is None:
+        c.execute("INSERT INTO category (name) VALUES (?)", (category,))
+        conn.commit()
+        # Fetch the new category id
+        c.execute("SELECT id FROM category WHERE name=?", (category,))
+        category_id = c.fetchone()
+    # Create ITEM
+    item = [name, category_id[0], image_hash]
+    c.execute("INSERT INTO items (name, category_id, image_name) VALUES (?, ?, ?)", item)
     conn.commit()
-    item_id = c.lastrowid
     conn.close()
-    return {"id": item_id, "name": name, "category": category, "image_filename": image_filename}
+    return {"message": f"item received: {name}, category: {category}, image: {image_filename}"}
 
-    # # Open items.json file
-    # with open('items.json', 'r') as file:
-    #     items = json.load(file)
-    # # Add new item to the items list
-    # items['items'].append({"name": name, "category": category, "image_filename" : image_filename})
-    # # Updated items back to file
-    # with open('items.json', 'w') as file:
-    #     json.dump(items, file)
-    # return {"message": f"item received: {name}, category: {category}, image: {image_filename}"}
-
+# Define a route for the "/items" URL with a GET method
 @app.get("/items")
 def get_items():
-    conn = sqlite3.connect('data_base')
+    conn = sqlite3.connect(db_path)
     c = conn.cursor()
-    c.execute("SELECT * FROM items")
-    items = [{"id": row[0], "name": row[1], "category": row[2], "image_filename": row[3]} for row in c.fetchall()]
+    c.execute("SELECT items.id, items.name, category.name, items.image_name\
+        from items INNER JOIN category ON items.category_id = category.id")
+    items = c.fetchall()
     conn.close()
-    return {"items": items}
-
-    # # Open the items.json file
-    # with open('items.json', 'r') as f:
-    #     items = json.load(f)
-    # return items
+    return items
 
 @app.get("/items/{item_id}")
 def get_item_id(item_id: int):
-    conn = sqlite3.connect('data_base')
+    conn = sqlite3.connect(db_path)
     c = conn.cursor()
-    c.execute("SELECT * FROM items WHERE id=?", (item_id,))
-    row = c.fetchone()
+    c.execute("SELECT items.id, items.name, category.name, items.image_name\
+        from items INNER JOIN category ON items.category_id = category.id")
+    items = c.fetchall()
+    conn.commit()
     conn.close()
-
-    if row:
-        return {"id": row[0], "name": row[1], "category": row[2], "image_filename": row[3]}
-    else:
-        raise HTTPException(status_code=404, detail="Item not found")
-
-    # with open('items.json','r') as file:
-    #     items = json.load(file)
-
-    # return items['items'][item_id]
+    return items[item_id - 1]
 
 @app.get("/image/{image_filename}")
 async def get_image(image_filename):
     # Create image path
     image = images / image_filename
-
     if not image_filename.endswith(".jpg"):
         raise HTTPException(status_code=400, detail="Image path does not end with .jpg")
-
     if not image.exists():
         logger.debug(f"Image not found: {image}")
         image = images / "default.jpg"
-
     return FileResponse(image)
 
+# search for items
 @app.get("/search")
-def search_items(keyword: str):
-    conn = sqlite3.connect(data_base)
+def search_item(keyword: str):
+    conn = sqlite3.connect(db_path)
     c = conn.cursor()
-    c.execute(f"SELECT * FROM items WHERE name = '{keyword}'")
-    items = c.fetchall()
-    conn.commit()
-    conn.close()
-    return items
+    search_keyword = f"%{keyword}%"
+    c.execute('''
+        SELECT items.id, items.name, category.name, items.image_name
+        FROM items
+        INNER JOIN category ON items.category_id = category.id
+        WHERE items.name LIKE ?
+    ''', (search_keyword,))
+    search_item = c.fetchall()
+    return search_item
